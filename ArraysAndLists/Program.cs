52eadd1c71f6243c0c5811045f542aaecc77c8a3
Andrea Schwenk <andreaschwenk3@gmail.com>
2022-03-29 11:29:39 -0500

    updated project

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -8,26 +8,35 @@ namespace ArraysAndLists
         static void Main(string[] args)
         {
             EvenOrOdd();
-            
         }
         public static void EvenOrOdd()
         {
 
             int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            List<int> even = new List<int>();  //Create two Lists of type int.
-            List<int> odd = new List<int>();  // Name one List "evens" * Name the other List "odd 
+            var even = new List<int>();  //Create two Lists of type int.
+             var odd = new List<int>();  // Name one List "evens" * Name the other List "odd 
 
 
-            foreach (var numbers in myArray)
+            for ( var i = 0; i < myArray.Length; i++)
             {
-                if ( numbers % 2 == 0)
+                if (myArray[i] % 2 == 0)
                 {
-                    Console.WriteLine("even");
+                    even.Add(myArray[i]);
                 }
                 else
-                    Console.WriteLine("odd");
+                    odd.Add(myArray[i]);
 
+            }
+            Console.WriteLine("even");
+            foreach (var num in even)
+            {
+                Console.WriteLine($"{num} is an even number");
+            }
+            Console.WriteLine("Odd");
+            foreach(var num in odd)
+            {
+                Console.WriteLine($"{num} is odd");
             }
         }
     }
